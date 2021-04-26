acda649b5a33eed74d6c42ae0900f6e86ca22c0a
Simon Petrac <pfoerdie@gmail.com>
2021-04-26 21:09:35 +0200

    lock.deep.hidden

--- src/prop.js
+++ src/prop.js
@@ -70,6 +70,24 @@ prop.lock.deep = function (obj, depth = Infinity) {
 };
 
 /**
+ * @param  {object} obj 
+ * @param  {number} [depth=Infinity] 
+ * @returns {prop}
+ */
+prop.lock.deep.hidden = function (obj, depth = Infinity) {
+    const config = { configurable: false, writable: false, enumerable: false };
+    for (let key of Object.keys(obj)) {
+        const writable = !obj.hasOwnProperty(key) || Reflect.getOwnPropertyDescriptor(obj, key).configurable;
+        if (writable) {
+            Object.defineProperty(obj, key, config);
+            if (obj[key] instanceof Object && depth > 0)
+                prop.lock.deep(obj[key], depth - 1);
+        }
+    }
+    return prop;
+};
+
+/**
  * @param {object} obj 
  * @param  {...(string|symbol)} keys 
  * @returns {prop}

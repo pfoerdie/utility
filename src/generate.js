1a8b92f30603e578edf62f9e15c243dcb90a0249
Simon Petrac <pfoerdie@gmail.com>
2021-05-24 20:02:23 +0200

    optimized the prime number generator to generate 10 million primes in 4.6 seconds on my machine

--- src/generate.js
+++ src/generate.js
@@ -22,31 +22,41 @@ generate.primes = function (length = 0) {
     if (length == 0) return primes;
     primes[0] = 2; // set the first prime for the algorithm to work
 
-    // NOTE buffer size maximum = 2 ** 32 - 1;
-    // const sieve_max_size = 6; // works with length = 10
-    const sieve_max_size = 2 ** 24 - 1; // good for length < 1 000 000
-    // const sieve_max_size = 2 ** 25 - 1; // good for length > 2 000 000
-    // IDEA find out a function for the optimal seive size at a given index
-    //      to improve performance even further for this algorithm
+    const
+        // sieve_max_size = 2 ** 24 - 1, // good for length < 1 000 000
+        // sieve_max_size = 2 ** 25 - 1, // good for length > 2 000 000
+        buffer_max_size = 2 ** 32 - 1;
+
     const not_prime = 0, is_prime = 1;
     let index = 1, lastPrime = 2, coveredMax = 3;
     while (index < length) {
-
         // 1. create an empty sieve of eratosthenes that extends
-        // from the covered maximum to the square of the last found prime
+        // from the covered maximum to at max the square of the last found prime
         // and initialize it with the is prime value
         const
             sieveOffset = coveredMax,
-            sieveSize = Math.min(lastPrime * lastPrime - sieveOffset, sieve_max_size),
-            // NOTE its the maximal possibly sieve size currently, not the optimal
-            sieveMax = sieveOffset + sieveSize;
-        const sieve = Buffer.alloc(sieveSize, is_prime);
+            sieveMaxGap = Math.ceil(lastPrime ** .75),
+            sieveMaxRange = lastPrime * lastPrime,
+            sieveMax = Math.min(Math.max(
+                // this method optimizes the sieve size for the given length and the current index
+                sieveOffset + 2 ** Math.floor(Math.log10(length - index)) * sieveMaxGap,
+                sieveOffset + sieveMaxGap), sieveMaxRange
+            ),
+            sieveSize = Math.min(sieveMax - sieveOffset, buffer_max_size);
+
+        // console.log(Object.entries({ index, lastPrime, sieveMax, sieveOffset, sieveSize })
+        //     .map(([key, value]) => key + ': \u001b[1m' + value.toLocaleString() + '\u001b[22m').join(', '));
+
+        const
+            sieve = Buffer.alloc(sieveSize, is_prime),
+            sieveCoveredMax = sieveOffset + sieveSize,
+            sieveLastPossiblePrime = Math.floor(Math.sqrt(sieveCoveredMax));
 
         // 2. mark all multiples of known primes in the sieve as not prime
         // and calculate the offset in the sieve array via modulo prime
         for (let i = 0; i < index; i++) {
             const prime = primes[i];
-            if (prime * prime > sieveMax) break; // NOTE might reduce the multiplication
+            if (prime > sieveLastPossiblePrime) break;
             for (let k = (- sieveOffset) % prime; k < sieveSize; k += prime) {
                 sieve[k] = not_prime;
             }
@@ -62,12 +72,17 @@ generate.primes = function (length = 0) {
                 if (index >= length) break;
             }
         }
-        if (index == lastIndex)
-            throw new Error('no new primes found in the sieve');
+        if (index == lastIndex) throw new Error('no new primes found in the sieve');
 
         // 4. update covered maximum for the next iteration
-        coveredMax = sieveMax;
+        coveredMax = sieveCoveredMax;
     }
 
     return primes;
 };
+
+// NOTE The current prime number generator is pretty good, it generates 10 million primes in 5 seconds.
+//      The implemented algorithm seems to be optimized as far as possible. But the question still remains,
+//      can a similar algorithm irradicate the flaws of this algorithm and do less unnessesary work
+//      by maybe copying buffers and be faster although the sieves have to overlap?
+//      Is there a clever way to manage this overlap during iteration? Then I could revert to a foor loop as previously!
--- test/develop.js
+++ test/develop.js
@@ -1,10 +1,19 @@
 const _ = require('@pfoerdie/utility');
-const PRIMES = 1e6;
+// const PRIMES = 2;
+// const PRIMES = 1e3;
+// const PRIMES = 1e6;
+const PRIMES = 10e6;
+// const PRIMES = 50e6;
 console.log('generating ' + PRIMES.toLocaleString() + ' prime numbers');
 console.time('time');
 try {
     const primes = _.generate.primes(PRIMES);
-    // console.log(primes.join(', '));
+    if (primes.length > 10) {
+        console.log('primes: [' + primes.slice(0, 5).join(', ') + ', ..., '
+            + primes.slice(primes.length - 5, primes.length).join(', ') + ']');
+    } else {
+        console.log('primes: [' + primes.join(', ') + ']');
+    }
 } catch (err) {
     console.error(err?.stack ?? err);
     // debugger

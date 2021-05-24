18c07b16ec7385146f868bd74a02dc5a990701bb
Simon Petrac <pfoerdie@gmail.com>
2021-05-24 23:48:27 +0200

    improved the algorithm even further with all previous techniques: 1mil primes in 240ms 10mil primes in 2.4s 50mil primes in 16s 100mil primes in 35s 1bil => primes are too large for int32 result array

--- src/generate.js
+++ src/generate.js
@@ -22,67 +22,56 @@ generate.primes = function (length = 0) {
     if (length == 0) return primes;
     primes[0] = 2; // set the first prime for the algorithm to work
 
-    const
-        // sieve_max_size = 2 ** 24 - 1, // good for length < 1 000 000
-        // sieve_max_size = 2 ** 25 - 1, // good for length > 2 000 000
-        buffer_max_size = 2 ** 32 - 1;
+    // 0. setup initial values for the algorithm
+    const not_prime = 0, is_prime = 1, max_buffer_size = 2 ** 32 - 1;
+    let index = 1, lastPrime = 2, lastOffset = 2, lastSieve = Buffer.alloc(1, is_prime);
 
-    const not_prime = 0, is_prime = 1;
-    let index = 1, lastPrime = 2, coveredMax = 3;
     while (index < length) {
-        // 1. create an empty sieve of eratosthenes that extends
-        // from the covered maximum to at max the square of the last found prime
-        // and initialize it with the is prime value
+        // 1. create a new sieve of eratosthenes and copy the overlap from the last sieve over
         const
-            sieveOffset = coveredMax,
-            sieveMaxGap = Math.ceil(lastPrime ** .75),
-            sieveMaxRange = lastPrime * lastPrime,
-            sieveMax = Math.min(Math.max(
-                // this method optimizes the sieve size for the given length and the current index
-                sieveOffset + 2 ** Math.floor(Math.log10(length - index)) * sieveMaxGap,
-                sieveOffset + sieveMaxGap), sieveMaxRange
+            offset = lastPrime + 1,
+            // this factor optimizes the sieve size for the given length and the current index
+            factor = 2 ** Math.max(0, Math.floor(Math.log10(length - index) - 2)),
+            size = Math.min(
+                // the factor must be at least 1 and will be multiplied 
+                // by the theoretical minimal gap size of the last prime to the next
+                factor * Math.ceil(lastPrime ** .75 + Number.EPSILON),
+                lastPrime ** 2 - offset,
+                max_buffer_size
             ),
-            sieveSize = Math.min(sieveMax - sieveOffset, buffer_max_size);
+            sieve = Buffer.alloc(size, is_prime),
+            overlap = lastSieve.copy(sieve, 0, offset - lastOffset),
+            overlapOffset = 0 - (offset + overlap);
 
-        // console.log(Object.entries({ index, lastPrime, sieveMax, sieveOffset, sieveSize })
-        //     .map(([key, value]) => key + ': \u001b[1m' + value.toLocaleString() + '\u001b[22m').join(', '));
-
-        const
-            sieve = Buffer.alloc(sieveSize, is_prime),
-            sieveCoveredMax = sieveOffset + sieveSize,
-            sieveLastPossiblePrime = Math.floor(Math.sqrt(sieveCoveredMax));
+        // NOTE logging of algorithm steps:
+        // console.log(Object.entries({ index, offset, overlap, size })
+        //     .map(([key, value]) => key + ': ' + value.toLocaleString()).join(', '));
 
         // 2. mark all multiples of known primes in the sieve as not prime
         // and calculate the offset in the sieve array via modulo prime
         for (let i = 0; i < index; i++) {
             const prime = primes[i];
-            if (prime > sieveLastPossiblePrime) break;
-            for (let k = (- sieveOffset) % prime; k < sieveSize; k += prime) {
+            for (let k = overlap + overlapOffset % prime, n = sieve.length - 1; k <= n; k += prime) {
                 sieve[k] = not_prime;
             }
         }
 
-        // 3. update covered maximum add all left primes in the sieve 
-        // to the primes array and increment the index
+        // 3. find all primes in the sieve
         const lastIndex = index;
-        for (let k = 0; k < sieveSize; k++) {
+        for (let k = 0, n = sieve.length - 1; k <= n; k++) {
             if (sieve[k] == is_prime) {
-                lastPrime = sieveOffset + k;
-                primes[index++] = lastPrime;
+                primes[index++] = offset + k;
                 if (index >= length) break;
             }
         }
-        if (index == lastIndex) throw new Error('no new primes found in the sieve');
+        if (index == lastIndex) throw new Error('found no new primes in the current sieve');
 
-        // 4. update covered maximum for the next iteration
-        coveredMax = sieveCoveredMax;
+        // 4. update last values for the next iteration
+        lastPrime = primes[index - 1];
+        if (lastPrime < 0) throw new Error('the primes are too large for the int32 result array');
+        lastOffset = offset;
+        lastSieve = sieve;
     }
 
     return primes;
 };
-
-// NOTE The current prime number generator is pretty good, it generates 10 million primes in 5 seconds.
-//      The implemented algorithm seems to be optimized as far as possible. But the question still remains,
-//      can a similar algorithm irradicate the flaws of this algorithm and do less unnessesary work
-//      by maybe copying buffers and be faster although the sieves have to overlap?
-//      Is there a clever way to manage this overlap during iteration? Then I could revert to a foor loop as previously!
--- test/develop.js
+++ test/develop.js
@@ -1,16 +1,16 @@
 const _ = require('@pfoerdie/utility');
-// const PRIMES = 2;
+// const PRIMES = 10;
 // const PRIMES = 1e3;
-// const PRIMES = 1e6;
-const PRIMES = 10e6;
-// const PRIMES = 50e6;
+// const PRIMES = 10e6;
+const PRIMES = 50e6;
+// const PRIMES = 1e9; // the larger primes are too large for the int32 result array
 console.log('generating ' + PRIMES.toLocaleString() + ' prime numbers');
 console.time('time');
 try {
     const primes = _.generate.primes(PRIMES);
     if (primes.length > 10) {
-        console.log('primes: [' + primes.slice(0, 5).join(', ') + ', ..., '
-            + primes.slice(primes.length - 5, primes.length).join(', ') + ']');
+        console.log('primes: [' + primes.slice(0, 7).join(', ') + ', ..., '
+            + primes.slice(primes.length - 3, primes.length).join(', ') + ']');
     } else {
         console.log('primes: [' + primes.join(', ') + ']');
     }

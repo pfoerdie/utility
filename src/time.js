348469c4091b94d36ca9e392a979e2ff29106c74
Simon Petrac <pfoerdie@gmail.com>
2021-11-16 23:20:02 +0100

    added docs

--- src/time.js
+++ src/time.js
@@ -1,55 +1,99 @@
 const _ = require('.');
 
+/**
+ * @param {number|string|Date} [value]
+ * @returns {string}
+ */
 const time = module.exports = function (value) {
     const date = _parseDate(value);
     return _stringifyTime(date);
 };
 
+/**
+ * @param {number|string|Date} [value]
+ * @returns {string}
+ */
 time.iso = function (value) {
     const date = _parseDate(value);
     return _stringifyTime(date) + _stringifyTimezone(date);
 };
 
+/**
+ * @param {number|string|Date} [value]
+ * @returns {string}
+ */
 time.utc = function (value) {
     const date = _parseDate(value);
     return _stringifyUtcTime(date) + 'Z';
 };
 
+/**
+ * @param {number|string|Date} [value]
+ * @returns {string}
+ */
 time.date = function (value) {
     const date = _parseDate(value);
     return _stringifyDate(date);
 };
 
+/**
+ * @param {number|string|Date} [value]
+ * @returns {string}
+ */
 time.date.iso = function (value) {
     const date = _parseDate(value);
     return _stringifyDate(date) + _stringifyTimezone(date);
 };
 
+/**
+ * @param {number|string|Date} [value]
+ * @returns {string}
+ */
 time.date.utc = function (value) {
     const date = _parseDate(value);
     return _stringifyUtcDate(date) + 'Z';
 };
 
+/**
+ * @param {number|string|Date} [value]
+ * @returns {string}
+ */
 time.datetime = function (value) {
     const date = _parseDate(value);
     return _stringifyDate(date) + 'T' + _stringifyTime(date);
 };
 
+/**
+ * @param {number|string|Date} [value]
+ * @returns {string}
+ */
 time.datetime.iso = function (value) {
     const date = _parseDate(value);
     return _stringifyDate(date) + 'T' + _stringifyTime(date) + _stringifyTimezone(date);
 };
 
+/**
+ * @param {number|string|Date} [value]
+ * @returns {string}
+ */
 time.datetime.utc = function (value) {
     const date = _parseDate(value);
     return _stringifyUtcDate(date) + 'T' + _stringifyUtcTime(date) + 'Z';
 };
 
+/**
+ * @param {number|string|Date} [value]
+ * @returns {Date}
+ */
 function _parseDate(value) {
     if (_.is.null(value)) return new Date();
     return new Date(value);
 }
 
+/**
+ * @param {Date} date
+ * @returns {string}
+ */
 function _stringifyTime(date) {
     const
         hour = date.getHours().toString().padStart(2, '0'),
@@ -60,6 +104,10 @@ function _stringifyTime(date) {
     return hour + ':' + minute + ':' + second + '.' + millisecond;
 }
 
+/**
+ * @param {Date} date
+ * @returns {string}
+ */
 function _stringifyUtcTime(date) {
     const
         hour = date.getUTCHours().toString().padStart(2, '0'),
@@ -70,6 +118,10 @@ function _stringifyUtcTime(date) {
     return hour + ':' + minute + ':' + second + '.' + millisecond;
 }
 
+/**
+ * @param {Date} date
+ * @returns {string}
+ */
 function _stringifyDate(date) {
     const
         year = date.getFullYear().toString(),
@@ -79,6 +131,10 @@ function _stringifyDate(date) {
     return year + '-' + month + '-' + day;
 }
 
+/**
+ * @param {Date} date
+ * @returns {string}
+ */
 function _stringifyUtcDate(date) {
     const
         year = date.getUTCFullYear().toString(),
@@ -88,6 +144,10 @@ function _stringifyUtcDate(date) {
     return year + '-' + month + '-' + day;
 }
 
+/**
+ * @param {Date} date
+ * @returns {string}
+ */
 function _stringifyTimezone(date) {
     const
         offset = date.getTimezoneOffset(),
